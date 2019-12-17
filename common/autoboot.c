29eff13fba1df667dd0cd0bf31a89ffd82b582ac
leijie <leijie@icoolarm.com>
2019-12-17 15:22:30 +0800

    修改自动烧录

--- common/autoboot.c
+++ common/autoboot.c
@@ -345,8 +345,8 @@ void autoboot_command(const char *s)
 
 	if (stored_bootdelay != -1 && s && !abortboot(stored_bootdelay)) {
 #ifdef GZYS_USBBURN
-	run_command("spi_nand probe 0");
-	run_command("spi_nand update 0x80000000 0 0x140000");
+	run_command("spi_nand probe 0",0);
+	run_command("spi_nand update 0x80000000 0 0x140000",0);
 #endif
 #if defined(CONFIG_AUTOBOOT_KEYED) && !defined(CONFIG_AUTOBOOT_KEYED_CTRLC)
 		int prev = disable_ctrlc(1);	/* disable Control C checking */
